ab3a34695fe926a680f06d11c65fa7adc249ee21
GuthrieW <wesleyeguthrie@gmail.com>
2020-07-29 16:01:49 -0500

    DB Connection solution

---
+++ config/database/database.js
@@ -0,0 +1,56 @@
+const mysql = require('mysql2');
+const { Client } = require('ssh2');
+
+const dbServer = {
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	user: process.env.DB_USERNAME,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE,
+};
+
+const tunnelConfig = {
+	host: process.env.SSH_HOST,
+	port: process.env.SSH_PORT,
+	username: process.env.SSH_USERNAME,
+	password: process.env.SSH_PASSWORD,
+};
+
+const forwardConfig = {
+	srcHost: '127.0.0.1',
+	srcPort: '3306',
+	dstHost: dbServer.host,
+	dstPort: dbServer.port,
+};
+
+const sshClient = new Client();
+
+const SSHConnection = new Promise((resolve, reject) => {
+	sshClient
+		.on('ready', () => {
+			sshClient.forwardOut(
+				forwardConfig.srcHost,
+				forwardConfig.srcPort,
+				forwardConfig.dstHost,
+				forwardConfig.dstPort,
+				(error, stream) => {
+					if (error) throw error;
+
+					const updatedDbServer = {
+						...dbServer,
+						stream,
+					};
+
+					const connection = mysql.createConnection(updatedDbServer);
+					connection.connect((error) => {
+						if (error) reject(error);
+
+						resolve(connection);
+					});
+				}
+			);
+		})
+		.connect(tunnelConfig);
+});
+
+module.exports = SSHConnection;
--- routes/api/v1/conferences.js
+++ routes/api/v1/conferences.js
@@ -1,30 +1,48 @@
 const express = require('express');
 const httpStatusCodes = require('http-status-codes');
 const router = express.Router();
-const dbConnection = require('./../../../database');
+// const dbConnection = require('./../../../config/database');
+const dbConnection = require('./../../../config/database/database');
 
 router.get(`/`, async (request, response) => {
-	dbConnection.query(
-		{
-			sql: 'SELECT * FROM conferences',
-			timeout: 40000,
-		},
-		[],
-		(error, results, fields) => {
-			console.log('fields', fields);
+	dbConnection().then((connection) => {
+		connection.query(
+			'SELECT * FROM conferences',
+			(error, results, fields) => {
+				if (error) {
+					response
+						.status(httpStatusCodes.INTERNAL_SERVER_ERROR)
+						.json(error);
+					console.log(error);
+					return;
+				}
 
-			if (error) {
-				console.error(error);
-				response
-					.status(httpStatusCodes.INTERNAL_SERVER_ERROR)
-					.json(error);
+				response.status(httpStatusCodes.OK).json(results);
 			}
+		);
+	});
 
-			response.status(httpStatusCodes.OK).json(results);
-		}
-	);
+	// dbConnection.query(
+	// 	{
+	// 		sql: 'SELECT * FROM conferences',
+	// 		timeout: 40000,
+	// 	},
+	// 	[],
+	// 	(error, results, fields) => {
+	// 		console.log('fields', fields);
 
-	dbConnection.end();
+	// 		if (error) {
+	// 			console.error(error);
+	// 			response
+	// 				.status(httpStatusCodes.INTERNAL_SERVER_ERROR)
+	// 				.json(error);
+	// 		}
+
+	// 		response.status(httpStatusCodes.OK).json(results);
+	// 	}
+	// );
+
+	// dbConnection.end();
 });
 
 module.exports = router;
