81b0c6844bdeeef0188b08a4fc86612716ddedc1
GuthrieW <wesleyeguthrie@gmail.com>
2020-07-30 19:49:37 -0500

    Fixed formatting and added variables

--- routes/api/v1/conferences.js
+++ routes/api/v1/conferences.js
@@ -1,43 +1,27 @@
-const express = require("express");
-const httpStatusCodes = require("http-status-codes");
+const express = require('express');
+const httpStatusCodes = require('http-status-codes');
 const router = express.Router();
 // const dbConnection = require('./../../../config/database');
-const dbConnection = require("./../../../config/database/database");
+const dbConnection = require('./../../../config/database/database');
 
 router.get(`/`, async (request, response) => {
-  dbConnection.then((connection) => {
-    connection.query("SELECT * FROM conferences", (error, results, fields) => {
-      if (error) {
-        response.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json(error);
-        console.log(error);
-        return;
-      }
+	const conferenceId = request.params.conferenceId;
+	const seasonId = request.params.seasonId || 54;
+	dbConnection.then((connection) => {
+		connection.query(
+			`SELECT * FROM conferences WHERE ConferenceID=${conferenceId} AND SeasonID=${seasonId}`,
+			(error, results, fields) => {
+				if (error) {
+					response
+						.status(httpStatusCodes.INTERNAL_SERVER_ERROR)
+						.json(error);
+					return;
+				}
 
-      response.status(httpStatusCodes.OK).json(results);
-    });
-  });
-
-  // dbConnection.query(
-  // 	{
-  // 		sql: 'SELECT * FROM conferences',
-  // 		timeout: 40000,
-  // 	},
-  // 	[],
-  // 	(error, results, fields) => {
-  // 		console.log('fields', fields);
-
-  // 		if (error) {
-  // 			console.error(error);
-  // 			response
-  // 				.status(httpStatusCodes.INTERNAL_SERVER_ERROR)
-  // 				.json(error);
-  // 		}
-
-  // 		response.status(httpStatusCodes.OK).json(results);
-  // 	}
-  // );
-
-  // dbConnection.end();
+				response.status(httpStatusCodes.OK).json(results);
+			}
+		);
+	});
 });
 
 module.exports = router;
