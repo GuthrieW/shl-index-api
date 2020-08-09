1171b9a503563eed85d98aefac7b6a1f38528902
GuthrieW <wesleyeguthrie@gmail.com>
2020-08-09 00:22:36 -0500

    schedule

--- routes/api/v1/schedule.js
+++ routes/api/v1/schedule.js
@@ -25,4 +25,50 @@ router.get(`/`, async (request, response) => {
 	});
 });
 
+router.get('/header', async (request, response) => {
+	dbConnection.then((connection) => {
+		const seasonId = request.query.seasonId;
+		const leagueId = request.query.leagueId;
+		0;
+		connection.query(
+			`SELECT * FROM schedules WHERE SeasonID=${
+				seasonId
+					? seasonId
+					: `(SELECT DISTINCT SeasonID FROM schedules WHERE LeagueID=${leagueId} ORDER BY SeasonID DESC LIMIT 1)`
+			} AND LeagueID=${leagueId}`,
+			(error, results, fields) => {
+				if (error) {
+					response
+						.status(httpStatusCodes.INTERNAL_SERVER_ERROR)
+						.json(error);
+					return;
+				}
+
+				response.status(httpStatusCodes.OK).json(results);
+			}
+		);
+	});
+});
+
+router.get('/', async (request, response) => {
+	dbConnection.then((connection) => {
+		const seasonId = request.query.seasonId;
+		const leagueId = request.query.leagueId;
+
+		connection.query(
+			`SELECT * FROM schedules WHERE SeasonId=${seasonId} AND LeagueId=${leagueId}`,
+			(error, results, fields) => {
+				if (error) {
+					response
+						.status(httpStatusCodes.INTERNAL_SERVER_ERROR)
+						.json(error);
+					return;
+				}
+
+				response.status(httpStatusCodes.OK).json(results);
+			}
+		);
+	});
+});
+
 module.exports = router;
