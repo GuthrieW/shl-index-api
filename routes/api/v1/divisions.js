dfc95913d009c9c6fc6bdacb915117ee8a071de4
Evan Silverman <es4753@nyu.edu>
2020-08-09 20:41:18 -0400

    Documentation and Modifications (#3)
    * Fix up some errors with db queries and add more endpoints
    
    * Add Documentation

--- routes/api/v1/divisions.js
+++ routes/api/v1/divisions.js
@@ -1,24 +1,55 @@
-const express = require('express');
-const httpStatusCodes = require('http-status-codes');
-const dbConnection = require('./../../../config/database/database');
+const express = require("express");
+const httpStatusCodes = require("http-status-codes");
+const dbConnection = require("./../../../config/database/database");
 const router = express.Router();
 
 router.get(`/`, async (request, response) => {
-	dbConnection.then((connection) => {
-		connection.query(
-			`SELECT * FROM divisions`,
-			(error, results, fields) => {
-				if (error) {
-					response
-						.status(httpStatusCodes.INTERNAL_SERVER_ERROR)
-						.json(error);
-					return;
-				}
+  const leagueId = request.query.leagueId ?? 0;
+  const conferenceId = request.query.conferenceId;
+  const seasonId = request.query.seasonId;
 
-				response.status(httpStatusCodes.OK).json(results);
-			}
-		);
-	});
+  dbConnection.then((connection) => {
+    connection.query(
+      `SELECT * FROM divisions WHERE LeagueID=${leagueId} AND SeasonID=${
+        seasonId
+          ? seasonId
+          : `(SELECT DISTINCT SeasonID FROM conferences WHERE LeagueID=${leagueId} ORDER BY SeasonID DESC LIMIT 1)`
+      } ${conferenceId ? `AND ConferenceID=${conferenceId}` : ``}`,
+      (error, results, fields) => {
+        if (error) {
+          response.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json(error);
+          return;
+        }
+
+        response.status(httpStatusCodes.OK).json(results);
+      }
+    );
+  });
+});
+
+router.get(`/:divisionId`, async (request, response) => {
+  const divisionId = request.params.divisionId;
+  const leagueId = request.query.leagueId ?? 0;
+  const conferenceId = request.query.conferenceId ?? 0;
+  const seasonId = request.query.seasonId;
+
+  dbConnection.then((connection) => {
+    connection.query(
+      `SELECT * FROM divisions WHERE LeagueID=${leagueId} AND ConferenceID=${conferenceId} AND DivisionID=${divisionId} AND SeasonID=${
+        seasonId
+          ? seasonId
+          : `(SELECT DISTINCT SeasonID FROM divisions WHERE LeagueID=${leagueId} ORDER BY SeasonID DESC LIMIT 1)`
+      }`,
+      (error, results, fields) => {
+        if (error) {
+          response.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json(error);
+          return;
+        }
+
+        response.status(httpStatusCodes.OK).json(results);
+      }
+    );
+  });
 });
 
 module.exports = router;
--- routes/api/v1/goalies.js
+++ routes/api/v1/goalies.js
@@ -1,27 +1,53 @@
-const express = require('express');
-const httpStatusCodes = require('http-status-codes');
-const dbConnection = require('./../../../config/database/database');
+const express = require("express");
+const httpStatusCodes = require("http-status-codes");
+const dbConnection = require("./../../../config/database/database");
 const router = express.Router();
 
 router.get(`/`, async (request, response) => {
-	dbConnection.then((connection) => {
-		const seasonType = request.query.seasonType;
-		const seasonId = request.query.seasonId;
-		const leagueId = request.query.leagueId;
-		connection.query(
-			`SELECT * FROM player_goalie_stats_${seasonType} WHERE SeasonID=${seasonId} AND LeagueID=${leagueId}`,
-			(error, results, fields) => {
-				if (error) {
-					response
-						.status(httpStatusCodes.INTERNAL_SERVER_ERROR)
-						.json(error);
-					return;
-				}
+  dbConnection.then((connection) => {
+    const seasonType = request.query.seasonType ?? "rs";
+    const seasonId = request.query.seasonId;
+    const leagueId = request.query.leagueId ?? 0;
+    connection.query(
+      `SELECT * FROM player_goalie_stats_${seasonType} WHERE SeasonID=${
+        seasonId
+          ? seasonId
+          : `(SELECT DISTINCT SeasonID FROM player_goalie_stats_${seasonType} WHERE LeagueID=${leagueId} ORDER BY SeasonID DESC LIMIT 1)`
+      } AND LeagueID=${leagueId}`,
+      (error, results, fields) => {
+        if (error) {
+          response.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json(error);
+          return;
+        }
 
-				response.status(httpStatusCodes.OK).json(results);
-			}
-		);
-	});
+        response.status(httpStatusCodes.OK).json(results);
+      }
+    );
+  });
+});
+
+router.get(`/:goalieId`, async (request, response) => {
+  dbConnection.then((connection) => {
+    const goalieId = request.params.goalieId;
+    const seasonType = request.query.seasonType ?? "rs";
+    const seasonId = request.query.seasonId;
+    const leagueId = request.query.leagueId ?? 0;
+    connection.query(
+      `SELECT * FROM player_goalie_stats_${seasonType} WHERE PlayerID=${goalieId} AND SeasonID=${
+        seasonId
+          ? seasonId
+          : `(SELECT DISTINCT SeasonID FROM player_goalie_stats_${seasonType} WHERE LeagueID=${leagueId} ORDER BY SeasonID DESC LIMIT 1)`
+      } AND LeagueID=${leagueId}`,
+      (error, results, fields) => {
+        if (error) {
+          response.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json(error);
+          return;
+        }
+
+        response.status(httpStatusCodes.OK).json(results);
+      }
+    );
+  });
 });
 
 module.exports = router;
