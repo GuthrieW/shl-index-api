94dd860a3527dd008421f385cda66d0c373ff6b2
Evan Silverman <es4753@nyu.edu>
2020-08-09 20:28:14 -0400

    Fix up some errors with db queries and add more endpoints

--- routes/api/v1/conferences.js
+++ routes/api/v1/conferences.js
@@ -1,26 +1,52 @@
-const express = require('express');
-const httpStatusCodes = require('http-status-codes');
-const dbConnection = require('./../../../config/database/database');
+const express = require("express");
+const httpStatusCodes = require("http-status-codes");
+const dbConnection = require("./../../../config/database/database");
 const router = express.Router();
 
 router.get(`/`, async (request, response) => {
-	const conferenceId = request.query.conferenceId;
-	const seasonId = request.query.seasonId || 54;
-	dbConnection.then((connection) => {
-		connection.query(
-			`SELECT * FROM conferences WHERE ConferenceID=${conferenceId} AND SeasonID=${seasonId}`,
-			(error, results, fields) => {
-				if (error) {
-					response
-						.status(httpStatusCodes.INTERNAL_SERVER_ERROR)
-						.json(error);
-					return;
-				}
+  const leagueId = request.query.leagueId ?? 0;
+  const seasonId = request.query.seasonId;
+  dbConnection.then((connection) => {
+    connection.query(
+      `SELECT * FROM conferences WHERE LeagueID=${leagueId} AND SeasonID=${
+        seasonId
+          ? seasonId
+          : `(SELECT DISTINCT SeasonID FROM conferences WHERE LeagueID=${leagueId} ORDER BY SeasonID DESC LIMIT 1)`
+      }`,
+      (error, results, fields) => {
+        if (error) {
+          response.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json(error);
+          return;
+        }
 
-				response.status(httpStatusCodes.OK).json(results);
-			}
-		);
-	});
+        response.status(httpStatusCodes.OK).json(results);
+      }
+    );
+  });
+});
+
+router.get(`/:conferenceId`, async (request, response) => {
+  const conferenceId = request.params.conferenceId;
+  const leagueId = request.query.leagueId ?? 0;
+  const seasonId = request.query.seasonId;
+
+  dbConnection.then((connection) => {
+    connection.query(
+      `SELECT * FROM conferences WHERE conferenceID=${conferenceId} AND LeagueID=${leagueId} AND SeasonID=${
+        seasonId
+          ? seasonId
+          : `(SELECT DISTINCT SeasonID FROM conferences WHERE LeagueID=${leagueId} ORDER BY SeasonID DESC LIMIT 1)`
+      }`,
+      (error, results, fields) => {
+        if (error) {
+          response.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json(error);
+          return;
+        }
+
+        response.status(httpStatusCodes.OK).json(results);
+      }
+    );
+  });
 });
 
 module.exports = router;
--- routes/api/v1/players.js
+++ routes/api/v1/players.js
@@ -1,81 +1,73 @@
-const express = require('express');
-const httpStatusCodes = require('http-status-codes');
-const dbConnection = require('./../../../config/database/database');
+const express = require("express");
+const httpStatusCodes = require("http-status-codes");
+const dbConnection = require("./../../../config/database/database");
 const router = express.Router();
 
 router.get(`/`, async (request, response) => {
-	dbConnection.then((connection) => {
-		connection.query(
-			`SELECT * FROM player_master`,
-			(error, results, fields) => {
-				if (error) {
-					response
-						.status(httpStatusCodes.INTERNAL_SERVER_ERROR)
-						.json(error);
-					return;
-				}
+  dbConnection.then((connection) => {
+    connection.query(
+      `SELECT * FROM player_master WHERE TeamID != -1`,
+      (error, results, fields) => {
+        if (error) {
+          response.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json(error);
+          return;
+        }
 
-				response.status(httpStatusCodes.OK).json(results);
-			}
-		);
-	});
+        response.status(httpStatusCodes.OK).json(results);
+      }
+    );
+  });
 });
 
 router.get(`/:id`, async (request, response) => {
-	dbConnection.then((connection) => {
-		const playerId = request.query.playerId;
-		connection.query(
-			`SELECT * FROM player_master WHERE PlayerId=${playerId}`,
-			(error, results, fields) => {
-				if (error) {
-					response
-						.status(httpStatusCodes.INTERNAL_SERVER_ERROR)
-						.json(error);
-					return;
-				}
+  dbConnection.then((connection) => {
+    const playerId = request.query.playerId;
+    connection.query(
+      `SELECT * FROM player_master WHERE PlayerId=${playerId}`,
+      (error, results, fields) => {
+        if (error) {
+          response.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json(error);
+          return;
+        }
 
-				response.status(httpStatusCodes.OK).json(results);
-			}
-		);
-	});
+        response.status(httpStatusCodes.OK).json(results);
+      }
+    );
+  });
 });
 
 router.get(`/:id/ratings`, async (request, response) => {
-	dbConnection.then((connection) => {
-		const playerId = request.query.playerId;
-		connection.query(
-			`SELECT * FROM player_ratins WHERE PlayerId=${playerId}`,
-			(error, results, fields) => {
-				if (error) {
-					response
-						.status(httpStatusCodes.INTERNAL_SERVER_ERROR)
-						.json(error);
-					return;
-				}
+  dbConnection.then((connection) => {
+    const playerId = request.query.playerId;
+    connection.query(
+      `SELECT * FROM player_ratings WHERE PlayerId=${playerId}`,
+      (error, results, fields) => {
+        if (error) {
+          response.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json(error);
+          return;
+        }
 
-				response.status(httpStatusCodes.OK).json(results);
-			}
-		);
-	});
+        response.status(httpStatusCodes.OK).json(results);
+      }
+    );
+  });
 });
 
 router.get(`/stats/:seasonType`, async (request, response) => {
-	dbConnection.then((connection) => {
-		const seasonType = request.query.seasonType;
-		connection.query(
-			`SELECT * FROM player_skate_stats_${seasonType}`,
-			(error, results, fields) => {
-				if (error) {
-					response
-						.status(httpStatusCodes.INTERNAL_SERVER_ERROR)
-						.json(error);
-					return;
-				}
+  dbConnection.then((connection) => {
+    const seasonType = request.query.seasonType;
+    connection.query(
+      `SELECT * FROM player_skate_stats_${seasonType}`,
+      (error, results, fields) => {
+        if (error) {
+          response.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json(error);
+          return;
+        }
 
-				response.status(httpStatusCodes.OK).json(results);
-			}
-		);
-	});
+        response.status(httpStatusCodes.OK).json(results);
+      }
+    );
+  });
 });
 
 module.exports = router;
